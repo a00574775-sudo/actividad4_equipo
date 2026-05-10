9edf27f89c94eb488de7162e760b9b8c1a1c6aa8
a00574775-sudo <ikerc@ROGIker.localdomain>
2026-05-09 20:53:23 -0600

    version modificada (cambio de color)

--- pong.py
+++ pong.py
@@ -1,4 +1,4 @@
-"""Pong, classic arcade game.
+g, classic arcade game.
 
 Exercises
 
@@ -6,10 +6,10 @@ Exercises
 2. What is the frame rate? Make it faster or slower.
 3. Change the speed of the ball.
 4. Change the size of the paddles.
-5. Change how the ball bounces off walls.
-6. How would you add a computer player?
-6. Add a second ball.
-"""
+5. Change how the ball bounces off walls
+
+
+"""Pong, classic arcade game - versión con colores personalizados."""
 
 from random import choice, random
 from turtle import *
@@ -48,17 +48,28 @@ def rectangle(x, y, width, height):
 
 def draw():
     """Draw game and move pong ball."""
+    
+    # Fondo negro
+    bgcolor("black")
+    
     clear()
+
+    # Color de las paletas
+    color("cyan")
     rectangle(-200, state[1], 10, 50)
+
+    color("magenta")
     rectangle(190, state[2], 10, 50)
 
     ball.move(aim)
     x = ball.x
     y = ball.y
 
+    # Color de la pelota
     up()
     goto(x, y)
-    dot(10)
+    dot(12, "yellow")
+
     update()
 
     if y < -200 or y > 200:
@@ -86,12 +97,120 @@ def draw():
 
 
 setup(420, 420, 370, 0)
+
+# Fondo inicial
+bgcolor("black")
+
 hideturtle()
 tracer(False)
+
 listen()
 onkey(lambda: move(1, 20), 'w')
 onkey(lambda: move(1, -20), 's')
 onkey(lambda: move(2, 20), 'i')
 onkey(lambda: move(2, -20), 'k')
+
+draw()
+done()"""Pong, classic arcade game - versión con colores personalizados."""
+
+from random import choice, random
+from turtle import *
+
+from freegames import vector
+
+
+def value():
+    """Randomly generate value between (-5, -3) or (3, 5)."""
+    return (3 + random() * 2) * choice([1, -1])
+
+
+ball = vector(0, 0)
+aim = vector(value(), value())
+state = {1: 0, 2: 0}
+
+
+def move(player, change):
+    """Move player position by change."""
+    state[player] += change
+
+
+def rectangle(x, y, width, height):
+    """Draw rectangle at (x, y) with given width and height."""
+    up()
+    goto(x, y)
+    down()
+    begin_fill()
+    for count in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+    end_fill()
+
+
+def draw():
+    """Draw game and move pong ball."""
+    
+    # Fondo negro
+    bgcolor("black")
+    
+    clear()
+
+    # Color de las paletas
+    color("cyan")
+    rectangle(-200, state[1], 10, 50)
+
+    color("magenta")
+    rectangle(190, state[2], 10, 50)
+
+    ball.move(aim)
+    x = ball.x
+    y = ball.y
+
+    # Color de la pelota
+    up()
+    goto(x, y)
+    dot(12, "yellow")
+
+    update()
+
+    if y < -200 or y > 200:
+        aim.y = -aim.y
+
+    if x < -185:
+        low = state[1]
+        high = state[1] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    if x > 185:
+        low = state[2]
+        high = state[2] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    ontimer(draw, 50)
+
+
+setup(420, 420, 370, 0)
+
+# Fondo inicial
+bgcolor("black")
+
+hideturtle()
+tracer(False)
+
+listen()
+onkey(lambda: move(1, 20), 'w')
+onkey(lambda: move(1, -20), 's')
+onkey(lambda: move(2, 20), 'i')
+onkey(lambda: move(2, -20), 'k')
+
 draw()
 done()
